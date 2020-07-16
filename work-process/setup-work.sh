12703690306b9f3892f5efd942e9f1b5fe3f5f76
David Philpott <david.philpott@sainsburys.co.uk>
2020-07-16 18:30:10 +0100

    Update project setup to new structure

--- work-process/setup-work.sh
+++ work-process/setup-work.sh
@@ -14,19 +14,19 @@ fi
 
 printf "Setting up new work project for '$PROJECT_NAME'."
 
-printf "\n\nCreate new Trello board with the name 'Work-$PROJECT_NAME'.\n"
+printf "\n\nCreate new Trello board with the name 'work-$PROJECT_NAME'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Evernote / Tusk notebook with the name 'Work-$PROJECT_NAME'.\n"
+printf "\n\nCreate new Evernote / Tusk notebook in the 'work' stack with the name '$PROJECT_NAME'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Dropbox folder with the name 'Work-$PROJECT_NAME'.\n"
+printf "\n\nCreate new Dropbox folder with the name '$PROJECT_NAME' in the 'work' folder.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Bitwarden folder with the name 'Work-$PROJECT_NAME'.\n"
+printf "\n\nCreate new Bitwarden folder with the name 'work-$PROJECT_NAME'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new folder on personal machine with the name 'Work-$PROJECT_NAME' in /home/.\n"
+printf "\n\nCreate new folder on personal machine with the name '$PROJECT_NAME' in /home/work.\n"
 read -s -p "Press Enter when complete."
 
 exit 0
