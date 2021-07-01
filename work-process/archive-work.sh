37fd2e77f5013cfdefe1da044da14a3aa05c0c92
David Philpott <david.philpott@convexin.com>
2021-07-01 09:41:25 +0100

    chore: convert in-script variables to lowercase

--- work-process/archive-work.sh
+++ work-process/archive-work.sh
@@ -5,52 +5,52 @@ while getopts p: option
 do
 case "${option}"
 in
-p) PROJECT_NAME=${OPTARG};;
+p) project_name=${OPTARG};;
 esac
 done
 
-if [ -z "$PROJECT_NAME" ]
+if [ -z "$project_name" ]
 then
       echo "No project name passed to script. Use -p argument."
       exit 1
 fi
 
-LOWER_PROJECT_NAME=${PROJECT_NAME,,}
-LOWER_DASH_PROJECT_NAME=${LOWER_PROJECT_NAME// /_}
+lower_project_name=${project_name,,}
+lower_dash_project_name=${lower_project_name// /_}
 
-printf "Archiving work project '$PROJECT_NAME'."
+printf "Archiving work project '$project_name'."
 
-printf "\n\nArchive Trello list in the 'Work' board with the name 'To Do - $PROJECT_NAME'.\n"
+printf "\n\nArchive Trello list in the 'Work' board with the name 'To Do - $project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nMove Evernote / Tusk notebook in the 'Work' stack with the name '$PROJECT_NAME' to 'Archived' stack.\n"
+printf "\n\nMove Evernote / Tusk notebook in the 'Work' stack with the name '$project_name' to 'Archived' stack.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nMove Dropbox folder inside the 'work' folder with the name '$LOWER_DASH_PROJECT_NAME' to 'archived' folder.\n"
+printf "\n\nMove Dropbox folder inside the 'work' folder with the name '$lower_dash_project_name' to 'archived' folder.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nRename Bitwarden folder with the name 'work-$LOWER_DASH_PROJECT_NAME' to 'archived-work-$LOWER_DASH_PROJECT_NAME'.\n"
+printf "\n\nRename Bitwarden folder with the name 'work-$lower_dash_project_name' to 'archived-work-$lower_dash_project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nMove folder on personal machine with the name '$LOWER_DASH_PROJECT_NAME' from /home/git/work/ to /home/git/archived/work/.\n"
+printf "\n\nMove folder on personal machine with the name '$lower_dash_project_name' from /home/git/work/ to /home/git/archived/work/.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nDelete firefox profile with the name '$PROJECT_NAME'.\n"
+printf "\n\nDelete firefox profile with the name '$project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nRemove personal shell config content relating to '$PROJECT_NAME'.\n"
+printf "\n\nRemove personal shell config content relating to '$project_name'.\n"
 printf "vim ~/.zshrc\n"
 read -s -p "Press Enter when complete." 
 
-printf "\n\nRemove personal aws config content relating to '$PROJECT_NAME'.\n"
+printf "\n\nRemove personal aws config content relating to '$project_name'.\n"
 printf "vim ~/.aws/config\n"
 printf "vim ~/.aws/credentials\n"
 read -s -p "Press Enter when complete." 
 
-printf "\n\nMove Boox folder for '$PROJECT_NAME' from 'Work' to 'Archived'.\n"
+printf "\n\nMove Boox folder for '$project_name' from 'Work' to 'Archived'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nMove ssh keys from ~/.ssh relating to '$PROJECT_NAME' into Bitwarden.\n"
+printf "\n\nMove ssh keys from ~/.ssh relating to '$project_name' into Bitwarden.\n"
 read -s -p "Press Enter when complete."
 
 exit 0
--- work-process/setup-work.sh
+++ work-process/setup-work.sh
@@ -5,40 +5,40 @@ while getopts p: option
 do
 case "${option}"
 in
-p) PROJECT_NAME=${OPTARG};;
+p) project_name=${OPTARG};;
 esac
 done
 
-if [ -z "$PROJECT_NAME" ]
+if [ -z "$project_name" ]
 then
       echo "No project name passed to script. Use -p argument."
       exit 1
 fi
 
-LOWER_PROJECT_NAME=${PROJECT_NAME,,}
-LOWER_DASH_PROJECT_NAME=${LOWER_PROJECT_NAME// /_}
+lower_project_name=${project_name,,}
+lower_dash_project_name=${lower_project_name// /_}
 
-printf "Setting up new work project for '$PROJECT_NAME'."
+printf "Setting up new work project for '$project_name'."
 
-printf "\n\nCreate new Trello list in the 'Work' board with the name 'To Do - $PROJECT_NAME'.\n"
+printf "\n\nCreate new Trello list in the 'Work' board with the name 'To Do - $project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Evernote / Tusk notebook in the 'work' stack with the name '$PROJECT_NAME'.\n"
+printf "\n\nCreate new Evernote / Tusk notebook in the 'work' stack with the name '$project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Dropbox folder with the name '$LOWER_DASH_PROJECT_NAME' in the 'work' folder.\n"
+printf "\n\nCreate new Dropbox folder with the name '$lower_dash_project_name' in the 'work' folder.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new Bitwarden folder with the name 'work-$LOWER_DASH_PROJECT_NAME'.\n"
+printf "\n\nCreate new Bitwarden folder with the name 'work-$lower_dash_project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate new folder on personal machine with the name '$LOWER_DASH_PROJECT_NAME' in /home/git/work.\n"
+printf "\n\nCreate new folder on personal machine with the name '$lower_dash_project_name' in /home/git/work.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate firefox profile with the name '$PROJECT_NAME'.\n"
+printf "\n\nCreate firefox profile with the name '$project_name'.\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nInstall the following firefox addons to profile '$PROJECT_NAME' and enable in private windows.\n"
+printf "\n\nInstall the following firefox addons to profile '$project_name' and enable in private windows.\n"
 printf "∙ uBlock Origin\n"
 printf "∙ Impulse Blocker\n"
 printf "∙ Privacy Badger\n"
@@ -47,7 +47,7 @@ printf "∙ HTTPs Everywhere\n"
 printf "∙ NoScript\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nConfigure the firefox profile '$PROJECT_NAME' with the following settings.\n"
+printf "\n\nConfigure the firefox profile '$project_name' with the following settings.\n"
 printf "∙ Set DuckDuckGo as default search engine\n"
 printf "∙ Disable 'Provide Search Suggestions'\n"
 printf "∙ 'Strict' Enhanced Tracking Protection\n"
@@ -57,7 +57,7 @@ printf "∙ Enable HTTPs Mode\n"
 printf "∙ Enable 'privacy.resistFingerprinting' in about:config\n"
 read -s -p "Press Enter when complete."
 
-printf "\n\nCreate folder named '$PROJECT_NAME' in the 'Work' folder on the Boox tablet.\n"
+printf "\n\nCreate folder named '$project_name' in the 'Work' folder on the Boox tablet.\n"
 read -s -p "Press Enter when complete." 
 
 exit 0
